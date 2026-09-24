92fafe869e9ca391cbe6347ca1bef8278a240b9c
soivi32423 <rtfftr131456@mail.ru>
2026-09-24 15:24:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart
-from aiogram.fstate import State, StatesGroup
+from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 from aiogram.types import (
     Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
